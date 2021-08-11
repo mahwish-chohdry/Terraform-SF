442c8167a45813de176e00009115933a39285f76
Mahwish Chohdry <mahwish@xavor.com>
2021-08-11 17:27:53 +0500

    test

--- main.tf
+++ main.tf
@@ -170,7 +170,7 @@ resource "azurerm_app_service" "APIapp" {
 
 
 resource "azurerm_app_service" "PortalApp" {
-  name                = "PortalTerraform"
+  name                = "Portal-Terraform"
   location            = azurerm_resource_group.RGroup.location
   resource_group_name = azurerm_resource_group.RGroup.name
   app_service_plan_id = azurerm_app_service_plan.PortalPlan.id
